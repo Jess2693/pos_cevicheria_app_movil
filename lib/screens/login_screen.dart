0327b6b784f38784e5f52c51b5062433ac82e07d
Jessenia <jessalexandra999@gmail.com>
2023-04-25 21:21:12 -0500

    add validator of email and password

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -42,43 +42,62 @@ class _loginForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                hintText: 'ejemplo@correo.com',
-                labelText: 'Correo: ',
-                prefixIcon: Icons.person_outline_sharp),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.visiblePassword,
-            decoration: InputDecorations.authInputDecoration(
-                hintText: '', labelText: 'Contraseña: ', prefixIcon: Icons.key),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          MaterialButton(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              color: AppTheme.primary,
-              elevation: 0,
-              child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                  child: const Text(
-                    'Entrar',
-                    style: TextStyle(color: AppTheme.tertiary),
-                  )),
-              onPressed: () {})
-        ],
-      )),
+            children: [
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: 'ejemplo@correo.com',
+                    labelText: 'Correo: ',
+                    prefixIcon: Icons.person_outline_sharp),
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regExp = new RegExp(pattern);
+                  return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'Correo incorrecto';
+                },
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                autocorrect: false,
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: '*****',
+                    labelText: 'Contraseña: ',
+                    prefixIcon: Icons.key),
+                validator: (value) {
+                  return (value != null && value.length >= 8)
+                      ? null
+                      : "Contraseña incorrecta";
+                },
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  color: AppTheme.primary,
+                  elevation: 0,
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 80, vertical: 15),
+                      child: const Text(
+                        'Entrar',
+                        style: TextStyle(color: AppTheme.tertiary),
+                      )),
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'home');
+                  })
+            ],
+          )),
     );
   }
 }
