1535203315bd7ba199068c1b38def748da3d8638
Jessenia <jessalexandra999@hotmail.com>
2023-04-25 22:36:55 -0500

    add form validator

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:pos_cevicheria/providers/login_form_proveider.dart';
 import 'package:pos_cevicheria/widget/widget.dart';
 import 'package:pos_cevicheria/ui/input_decoration.dart';
 import 'package:pos_cevicheria/theme/app_theme.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -26,7 +28,10 @@ class LoginScreen extends StatelessWidget {
                 const SizedBox(
                   height: 10,
                 ),
-                _loginForm()
+                ChangeNotifierProvider(
+                  create: (_) => LoginFormProvider(),
+                  child: _loginForm(),
+                ),
               ],
             ),
           ),
@@ -40,8 +45,11 @@ class LoginScreen extends StatelessWidget {
 class _loginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final loginForm = Provider.of<LoginFormProvider>(context);
+
     return Container(
       child: Form(
+          key: loginForm.formKey,
           autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
             children: [
@@ -52,6 +60,7 @@ class _loginForm extends StatelessWidget {
                     hintText: 'ejemplo@correo.com',
                     labelText: 'Correo: ',
                     prefixIcon: Icons.person_outline_sharp),
+                onChanged: (value) => loginForm.email = value,
                 validator: (value) {
                   String pattern =
                       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
@@ -70,8 +79,9 @@ class _loginForm extends StatelessWidget {
                 keyboardType: TextInputType.visiblePassword,
                 decoration: InputDecorations.authInputDecoration(
                     hintText: '*****',
-                    labelText: 'Contraseña : ',
+                    labelText: 'Contraseña: ',
                     prefixIcon: Icons.key),
+                onChanged: (value) => loginForm.password = value,
                 validator: (value) {
                   return (value != null && value.length >= 8)
                       ? null
@@ -94,7 +104,8 @@ class _loginForm extends StatelessWidget {
                         style: TextStyle(color: AppTheme.tertiary),
                       )),
                   onPressed: () {
-                    Navigator.pushNamed(context, 'home');
+                    if (!loginForm.isValidForm()) return;
+                    Navigator.pushReplacementNamed(context, 'home');
                   })
             ],
           )),
