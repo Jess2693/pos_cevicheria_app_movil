ef1215630ac5a16ea29e41c6ea48f55a7407f1a2
Jessenia <jessalexandra999@hotmail.com>
2023-04-27 01:02:17 -0500

    create home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:pos_cevicheria/theme/app_theme.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Inicio'),
+        backgroundColor: AppTheme.primary,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(Icons.account_circle_outlined),
+          )
+        ],
+        leading: const Icon(Icons.menu),
+      ),
+      body: const Center(
         child: Text('HomeScreen'),
       ),
     );
