a415c1195c97ba256e90f40f6dfc46c91c47c1b2
Jessenia <jessalexandra999@hotmail.com>
2023-05-08 16:34:16 -0500

    add appbar on screen

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:pos_cevicheria/theme/app_theme.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: GestureDetector(
+          onTap: () {},
+          child: const Icon(Icons.arrow_back, color: AppTheme.primary),
+        ),
+        backgroundColor: AppTheme.tertiary,
+        title: const Text(
+          "Nueva orden",
+          style: TextStyle(color: AppTheme.primary),
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(Icons.payments_outlined, color: AppTheme.primary),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(Icons.save_outlined, color: AppTheme.primary),
+          )
+        ],
+      ),
+    );
   }
 }
